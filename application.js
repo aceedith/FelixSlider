b4eb8bfb8ea16fc760e4e8be37fee765c78abda0
Felix Sungchul Kang <kormandk@naver.com>
2012-08-08 10:59:53 +1000

    Version 1.1 Update

---
+++ application.js
@@ -0,0 +1,65 @@
+$(function (){
+	prettyPrint();
+
+	$('#slider1').felixSlider();
+
+	$('#example1').felixSlider();
+	$('#example2').felixSlider({
+		auto: true,
+		rolling: true
+	});
+	$('#example3').felixSlider({
+		auto: true,
+		autoReverse: true,
+		rolling: true
+	});
+	$('#example4').felixSlider({
+		auto: true,
+		rolling: true,
+		intervalType: 'image'
+	});
+	$('#example5').felixSlider({
+		auto: true,
+		rolling: true,
+		showImageCount: 3
+	});
+	$('#example6').felixSlider({
+		auto: true,
+		rolling: true,
+		padding: 0
+	});
+	$('#example7').felixSlider({
+		auto: true,
+		rolling: true,
+		direction: 'V'
+	});
+	$('#example8').felixSlider({
+		auto: true,
+		rolling: true,
+		showImageCount: 3,
+		buttonShow: false
+	});
+	$('#ex8-pre').click(function (){
+		$('#example8').felixSlider('previous');
+	});
+	$('#ex8-next').click(function (){
+		$('#example8').felixSlider('next');
+	});
+	$('#example9').felixSlider({
+		auto: true,
+		rolling: true,
+		showImageCount: 3,
+		imageClick: function (target){
+			$('#ex9-preview').empty();
+			$('#ex9-preview').append(target);
+		}
+	});
+	$('#example10').felixSlider({
+		auto: true,
+		rolling: true,
+		showImageCount: 1,
+		buttonShow: false,
+		padding: 0
+	});
+
+});
